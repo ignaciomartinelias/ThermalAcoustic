cacff250dd180840fe8fd60ae9070ce4ec4ef128
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2018-11-01 01:55:22 -0300

    Initial commit

---
+++ js/carrusel.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.carrusel-wrapper').slick({
+    autoplay: true,
+    slidesToShow: 3,
+  	slidesToScroll: 1,
+  	autoplay: true,
+  	autoplaySpeed: 2000,
+  });
+});
