9655833ce37a6fe5bdd102efd5bf8f89320037b2
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2019-01-12 01:26:54 -0300

    Eliminar previos

--- js/carrusel.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.carrusel-wrapper').slick({
-    autoplay: true,
-    slidesToShow: 3,
-  	slidesToScroll: 1,
-  	autoplay: true,
-  	autoplaySpeed: 2000,
-  });
-});
