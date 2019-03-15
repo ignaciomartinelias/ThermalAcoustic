dc85b17b652d5279a2b2495532b6fcf839c9655a
Ignacio Martin Elias <ignaciomartinelias@gmail.com>
2019-03-15 15:20:37 -0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 // Carga de Productos al Showcase
 // Seteo de Background Image de Cada Product Showcase
 
-$.getJSON("/productos.json", function(data) {
+$.getJSON("/ThermalAcoustic/js/productos.json", function(data) {
   var items = [];
   $.each(data, function(key, val) {
     items.push(val);
